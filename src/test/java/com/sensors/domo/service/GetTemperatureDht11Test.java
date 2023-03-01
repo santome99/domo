ec14c56cefcd389fd4c6324389c64cad3d3c5118
rocafort99 <79190143+diego777183@users.noreply.github.com>
2023-03-01 13:27:30 +0100

    b

---
+++ src/test/java/com/sensors/domo/service/GetTemperatureDht11Test.java
@@ -0,0 +1,14 @@
+package com.sensors.domo.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GetTemperatureDht11Test {
+
+    @Test
+    void getTemperature2() {
+        Assertions.assertEquals(1, 1);
+    }
+}
